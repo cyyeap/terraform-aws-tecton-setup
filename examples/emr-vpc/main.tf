abf7f0c18abda6a4d73d059346d2b8683e4f9e33
Scott Ames <scott@tecton.ai>
2022-07-25 09:27:05 -0700

    feat: add emr notebook cluster (#4)
    # What
    
    Adds the EMR notebook cluster module + examples. Taken from [tecton-ai-ext/tecton-terraform-setup](https://github.com/tecton-ai-ext/tecton-terraform-setup/blob/master/emr_sample/infrastructure.tf#L59-L90)
    
    # Why
    
    EMR customers will need to deploy the notebook cluster upon the Tecton cluster being stood up.

--- examples/emr-vpc/main.tf
+++ examples/emr-vpc/main.tf
@@ -13,3 +13,31 @@ module "tecton" {
     aws = aws
   }
 }
+
+variable "enable_notebook_cluster" {
+  default     = false
+  description = <<EOV
+* This can not be enabled until the Tecton cluster is fully provisioned on the Tecton side *
+Toggle enabling the EMR notebook cluster. Default: false.
+EOV
+}
+
+module "notebook_cluster" {
+  source = "../../modules/emr-notebook-cluster"
+
+  count = var.enable_notebook_cluster ? 1 : 0
+
+  deployment_name           = "<DEPLOYMENT_NAME>"
+  subnet_ids                = module.tecton.private_subnet_ids
+  instance_profile_arn      = module.tecton.spark_instance_profile_arn
+  service_role_id           = module.tecton.master_role_name
+  cluster_security_group_id = module.tecton.security_group_ids.emr.cluster
+  service_security_group_id = module.tecton.security_group_ids.emr.service
+
+  // Optional
+  enable_glue = true
+
+  providers = {
+    aws = aws
+  }
+}
